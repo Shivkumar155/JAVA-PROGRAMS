a3fc1326be06f3bd5cb57d6a70aff1e3f9275902
Shiv Kumar Sharma <sharmakumarshiv7987@gmail.com>
2023-02-15 20:40:20 +0530

    Create FUNCTION_CALLING.java

---
+++ BASIC PROGRAMS/FUNCTION_CALLING.java
@@ -0,0 +1,14 @@
+
+                                                                     /*function calling*/
+public class ExFunc
+{
+	void f1()
+	{
+		System.out.println("Function is called");
+	}
+	public static void main(String[] args)
+	{
+		ExFunc obj = new ExFunc();
+		obj.f1();
+	}
+}
