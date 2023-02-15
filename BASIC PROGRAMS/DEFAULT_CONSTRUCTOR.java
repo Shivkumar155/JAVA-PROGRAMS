c6caacdb8f5a37503e7e920b1e9eb269f285fd46
Shiv Kumar Sharma <sharmakumarshiv7987@gmail.com>
2023-02-15 22:58:14 +0530

    Create DEFAULT_CONSTRUCTOR.java

---
+++ BASIC PROGRAMS/DEFAULT_CONSTRUCTOR.java
@@ -0,0 +1,16 @@
+
+                                                          /* Concept of default constructor */
+
+public class Exconst
+{
+	int x;
+	Exconst()
+	{
+		x=10;
+		System.out.println("value of x is : "+x);
+	}
+	public static void main(String[] args)
+	{
+		Exconst obj = new Exconst();
+	} 
+}
