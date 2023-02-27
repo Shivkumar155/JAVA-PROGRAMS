eda04d8d44342c66f4d895fd7fe14ed0b9f14ea5
Shiv Kumar Sharma <sharmakumarshiv7987@gmail.com>
2023-02-27 09:23:13 +0530

    Create CREATING_FRAME.java

---
+++ AWT/CREATING_FRAME.java
@@ -0,0 +1,12 @@
+
+                                                                              /* Date-> 21/02/23 */
+
+import java.awt.*;
+public class AwtFrame extends Frame{
+    public static void main(String[] args) {
+        AwtFrame obj = new AwtFrame();
+        obj.setTitle("hello");
+        obj.setSize(400,400);
+        obj.setVisible(true);
+    }
+}
